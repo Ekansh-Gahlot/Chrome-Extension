e4c7a033ef2156fda728655adfdb64bef5d3ead7
Ekansh-Gahlot <ekansh22gahlot@gmail.com>
2024-07-03 22:47:43 +0800

    Template string

--- index.js
+++ index.js
@@ -14,13 +14,34 @@ function renderLeads(){
     let listItems = ""
     for(let i = 0; i < myLeads.length;i++)
     {
-        //First method to add HTML element
-        listItems += "<li>" + myLeads[i] + "</li>"
+        //First method to add HTML element 
+        //listItems += "<li><a target = '_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
+        //console.log(listItems)
         
+
+
         //Second method to add HTML element
         /*const li = document.createElement("li")
         li.textContent = myLeads[i]
         ulEl.append(li)*/
+        // const li = document.createElement("li");
+        // const a = document.createElement("a");
+        // a.textContent = myLeads[i];
+        // a.href = myLeads[i]; // Assuming myLeads[i] contains the URL
+
+        // li.appendChild(a); // Append the anchor element to the list item
+        // ulEl.appendChild(li); // Append the list item to the unordered list
+
+
+        //USING TEMPLATE STRING TO SOLVE LISTITEMS LENGTH ISSUE MORE READABLE
+        listItems += `
+            <li>
+                <a target = '_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+        `
+
     
     }
     
